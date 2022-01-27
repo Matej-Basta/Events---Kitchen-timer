4942cb684a43491b697a4a0447cd2eb3cfdcde2e
Matej-Basta <71135305+Matej-Basta@users.noreply.github.com>
2022-01-27 17:53:41 +0100

    Added comments

--- script.js
+++ script.js
@@ -1,24 +1,31 @@
+//accessing 2 paragraphs
 const time = document.getElementById("time");
 const message = document.getElementById("message");
 
+//accessing the button
 const button = document.getElementById("button");
 
+//adding a event, when the button is clicked, the values from Minutes and Seconds are taken
 button.addEventListener("click", () => {
   let min = Number(document.getElementById("minutes").value);
   let sec = Number(document.getElementById("seconds").value);
+  //total is necessary for the setTimeout, to know when the Interval should be stopped and when to display a message
   let total = sec + min * 60;
-  console.log(total);
+  //initial text
   time.innerHTML = min + " : " + sec;
+  //interval - 1 second is subtracted and the text is updated every second
   const interval = setInterval(() => {
     sec--;
     time.innerHTML = min + " : " + sec;
 
+    //if the seconds reach 0 and there are still some minutes, the seconds must go back to 60 and 1 minute is subtracted
     if (sec === 0 && min !== 0) {
       sec = 60;
       min--;
     }
   }, 1000);
 
+  //timeout - showing final message and stopping the interval
   setTimeout(() => {
     message.innerHTML = "The time's up.";
     clearInterval(interval);
